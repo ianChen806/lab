aefc06a6d734376987793eb60d107d9d046b0061
ian <rutsuki123@gmail.com>
2019-03-14 11:00:39 +0800

    test

--- Lab/ProductValidator.cs
+++ Lab/ProductValidator.cs
@@ -4,6 +4,7 @@ namespace Lab
 {
     public class ProductValidator : IValidator<Product>
     {
+        //
         public bool Validate(Product model)
         {
             return model.Price - model.Cost >= 0;
