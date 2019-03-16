63a8789dde0d0a624309735ef1a47d4c8fb63cbd
Ian <rutsuki123@hotmail.com>
2019-03-16 12:01:27 +0800

    sequenceEqual

--- CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
+++ CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
@@ -18,9 +17,74 @@ namespace CSharpAdvanceDesignTests
             Assert.IsTrue(actual);
         }
 
+        [Test]
+        public void compare_two_numbers_equal_2()
+        {
+            var first = new List<int> { 3, 2, 1, 2 };
+            var second = new List<int> { 3, 2, 1 };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_equal_3()
+        {
+            var first = new List<int> { 3, 2, 12 };
+            var second = new List<int> { 3, 2, 1 };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_equal_4()
+        {
+            var first = new List<int> { 3, 2, 1 };
+            var second = new List<int> { 3, 2, 1, 0 };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_equal_5()
+        {
+            var first = new List<int> { };
+            var second = new List<int> { };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsTrue(actual);
+        }
         private bool JoeySequenceEqual(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new NotImplementedException();
+            var firstEnumerator = first.GetEnumerator();
+            var secondEnumerator = second.GetEnumerator();
+
+            while (true)
+            {
+                var firstFlag = firstEnumerator.MoveNext();
+                var secondFlag = secondEnumerator.MoveNext();
+
+                if (firstFlag != secondFlag)
+                {
+                    return false;
+                }
+
+                if (firstEnumerator.Current != secondEnumerator.Current)
+                {
+                    return false;
+                }
+
+                if (firstFlag == false)
+                {
+                    return true;
+                }
+            }
         }
     }
 }
